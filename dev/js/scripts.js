7d96741b774db367bf3fd642dc9cdf33200bae2f
Jadedinhh <mbd77@drexel.edu>
2022-04-14 19:26:29 -0400

    challenge1 complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,3 +5,15 @@ const yourName = "Jade Dinh"
 document.querySelector("#name").innerHTML = yourName;
 
 //GreenSock Timeline
+
+const firstTL = gsap.timeline();
+
+firstTL.from(".red", {duration:1, x:0, ease:"power1.out"}, "box")
+.from(".blue", {duration:1, x:350, ease:"power1.out"}, "box");
+
+
+const first2TL = gsap.timeline({delay:1});
+first2TL.to(".red", {duration:2, y:0, ease:"power1.out"},"box")
+.to(".blue", {duration:2, y:300, ease:"power1.out"}, "box");
+
+
