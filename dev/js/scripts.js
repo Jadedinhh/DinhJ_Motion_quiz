5976ec4d973221ff81df6410c822b8f4b0116c85
Jadedinhh <mbd77@drexel.edu>
2022-04-14 20:15:28 -0400

    bouncing

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,12 +9,12 @@ document.querySelector("#name").innerHTML = yourName;
 const firstTL = gsap.timeline();
 
 firstTL.from(".graph", {duration:1, opacity:0.25}, "box")
-.from(".red", {duration:1, x:0, ease:"power1.inOut"}, "box")
-.from(".blue", {duration:1, x:350,  ease:"power1.inOut"}, "box");
+.from(".red", {duration:1, x:0, ease:"bounce.out"}, "box")
+.from(".blue", {duration:1, x:350,  ease:"bounce.out"}, "box");
 
 
 const first2TL = gsap.timeline({delay:2});
 first2TL.to(".red", {duration:2, y:0, ease:"power1.out", rotate: 360},"box")
 .to(".blue", {duration:2, y:300, ease:"power1.out", rotate:360}, "box");
 
-gsap.from(".green", {duration:5, transformOrigin: "0 100%"});
+gsap.to(".green", {duration:5, transformOrigin: "left"});
