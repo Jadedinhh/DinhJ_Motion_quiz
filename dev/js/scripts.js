206e6930be5dd68b780681138816160a853e9516
Jadedinhh <mbd77@drexel.edu>
2022-04-14 20:19:59 -0400

    nice

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,11 +10,12 @@ const firstTL = gsap.timeline();
 
 firstTL.from(".graph", {duration:1, opacity:0.25}, "box")
 .from(".red", {duration:1, x:0, ease:"bounce.out"}, "box")
-.from(".blue", {duration:1, x:350,  ease:"bounce.out"}, "box");
+.from(".blue", {duration:1, x:350,  ease:"bounce.out"}, "box")
+.from(".green", {duration:5, transformOrigin: "0 0"}, "box");
 
 
 const first2TL = gsap.timeline({delay:2});
 first2TL.to(".red", {duration:2, y:0, ease:"power1.out", rotate: 360},"box")
 .to(".blue", {duration:2, y:300, ease:"power1.out", rotate:360}, "box");
 
-gsap.to(".green", {duration:5, transformOrigin: "left"});
+gsap.to(".green", {duration:5, transformOrigin: "0 0"});
