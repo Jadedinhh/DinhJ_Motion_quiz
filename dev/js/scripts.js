606d071b62fc7cb3d8d7e478efae0a972f729c92
Jadedinhh <mbd77@drexel.edu>
2022-04-14 20:05:47 -0400

    itried

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,13 +8,13 @@ document.querySelector("#name").innerHTML = yourName;
 
 const firstTL = gsap.timeline();
 
-firstTL.from(".graph", {duration:2, opacity:0.25}, "box")
-.from(".red", {duration:1, x:0}, "box")
-.from(".blue", {duration:1, x:350}, "box");
+firstTL.from(".graph", {duration:1, opacity:0.25}, "box")
+.from(".red", {duration:1, x:0, ease:"power1.inOut"}, "box")
+.from(".blue", {duration:1, x:350,  ease:"power1.inOut"}, "box");
 
 
 const first2TL = gsap.timeline({delay:2});
-first2TL.to(".red", {duration:2, y:0, ease:"power1.out"},"box")
-.to(".blue", {duration:2, y:300, ease:"power1.out"}, "box");
+first2TL.to(".red", {duration:2, y:0, ease:"power1.out", rotate: 360},"box")
+.to(".blue", {duration:2, y:300, ease:"power1.out", rotate:360}, "box");
 
-
+gsap.from(".green", {duration:5, transformOrigin: "0 100%"});
